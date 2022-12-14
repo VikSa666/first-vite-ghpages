635b50a5fa1413c7c42cf74d1c013ea9a87b23f1
victor-at-wiris <victor.santiago@wiris.com>
2022-12-14 08:14:54 +0100

    chore

--- src/components/MyFirstComponent.vue
+++ src/components/MyFirstComponent.vue
@@ -8,14 +8,14 @@ const name = ref('')
 const input_content = ref('')
 const input_category = ref(null)
 
-const todos_asc = computed(() => todos.value.sort((a,b) => {
+const todos_asc = computed(() => todos.value.sort((a, b) => {
     return b.createdAt - a.createdAt
 }))
 
 const addTodo = () => {
     if (input_content.value.trim() === '' || input_category.value === null) {
         return
-    } 
+    }
 
     console.log("addTodo");
 
@@ -27,7 +27,7 @@ const addTodo = () => {
 }
 
 const removeTodo = todo => {
-    todos.value = todos.value.filter(t => t!= todo)
+    todos.value = todos.value.filter(t => t != todo)
 }
 
 // Watching this name: if this changes, then update it in localStorage
@@ -52,12 +52,8 @@ watch(todos, newValue => {
     <main class="app">
         <section class="greeting">
             <h2 class="title">
-                What's up, 
-                <input 
-                    type="text" 
-                    placeholder="Name here" 
-                    v-model="name" 
-                />
+                What's up,
+                <input type="text" placeholder="Name here" v-model="name" />
             </h2>
         </section>
 
@@ -66,33 +62,19 @@ watch(todos, newValue => {
 
             <form @submit.prevent="addTodo">
                 <h4>What's on your todo list?</h4>
-                <input 
-                    type="text" 
-                    placeholder="e.g. make a video" 
-                    v-model="input_content"
-                />
+                <input type="text" placeholder="e.g. make a video" v-model="input_content" />
 
                 <h4>Pick a category</h4>
 
                 <div class="options">
                     <label>
-                        <input 
-                            type="radio"
-                            name="category"
-                            value="business"
-                            v-model="input_category"
-                        >
+                        <input type="radio" name="category" value="business" v-model="input_category">
                         <span class="bubble business"></span>
                         <div>Business</div>
                     </label>
 
                     <label>
-                        <input 
-                            type="radio"
-                            name="category"
-                            value="personal"
-                            v-model="input_category"
-                        >
+                        <input type="radio" name="category" value="personal" v-model="input_category">
                         <span class="bubble business"></span>
                         <div>Personal</div>
                     </label>
